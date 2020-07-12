bb505e6445ea033d33225272139e103a931d4217
jessiepoo <jessicahuang1353@hotmail.com>
2020-07-12 09:53:14 -0700

    jquery added animations fixed

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+ // piece of gode that does one or more actions
+ // do not repeat yourself DRY
+
+var myList = ['hey', 'what\'s up', 'hello'];
+
+for (var i=0; i<myList.length; i++) {
+    console.log(i, myList[i]);
+}
